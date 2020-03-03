c9df0d85cf3a5a0d568acd1dbe2d37a7717cd5de
EdwinDZ <edwin.dzemovski@ynov.com>
2020-03-03 19:59:28 +0100

    first commit

---
+++ src/components/search/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './search.css'
+
+const Search = () => {
+    return (
+        <div className='ujU zI7 iyn Hsu'>
+            <div role='button'>
+                <div className='DUt Jea Zr3 gjz gpV ujU zI7 iyn Hsu'>
+                    <div className='JME zI7 iyn Hsu'>
+                        <div className='tBJ dyH iFc SMy _S5 B9u DrD IZT mWe'>
+                            Rechercher
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default Search;
